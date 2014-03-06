b83ab062f5625fb5c4e0f77a3b049e362df664c9
Yegor Bugayenko <yegor@tpc2.com>
2014-03-06 16:42:56 +0100

    cron.sh fix for multiple runs

--- cron.sh
+++ cron.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+set -e
+set -o pipefail
 
 git pull
+DB=${HOME}/twitter.db
+OAUTH=$(cat ${HOME}/twitter.key)
+mvn clean package -Pliquibase -Dsqlite.file=${DB}
 
 CITIES=(
     "52.3740300,4.8896900,30km"
@@ -15,7 +20,7 @@ KEYWORDS=(
 for keyword in "${KEYWORDS[@]}"
     for city in "${CITIES[@]}"
     do
-        mvn test -DskipTests -Plive -Pcuriost \
-            -Dcity=${city} -Dtag=${KEYWORD} -Dsqlite.file=${HOME}/twitter.db
+        java -jar target/circles.jar --city=${city} \
+            --tag=${KEYWORD} --jdbc=jdbc:sqlite:${DB} --key=${OAUTH}
     done
 done
