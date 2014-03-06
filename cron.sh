e7c95edcb9237e8e86602405940baef98550fb41
Yegor Bugayenko <yegor@tpc2.com>
2014-03-06 16:34:51 +0100

    assembly:single

--- cron.sh
+++ cron.sh
@@ -6,8 +6,16 @@ CITIES=(
     "52.3740300,4.8896900,30km"
     "37.4419444,-122.1419444,50mi"
 )
-for city in "${CITIES[@]}"
-do
-    mvn test -DskipTests -Plive -Pcuriost \
-        -Dcity=${city} -Dtag=java -Dsqlite.file=${HOME}/twitter.db
+KEYWORDS=(
+    "java"
+    "github"
+    "aws"
+    "dynamo"
+)
+for keyword in "${KEYWORDS[@]}"
+    for city in "${CITIES[@]}"
+    do
+        mvn test -DskipTests -Plive -Pcuriost \
+            -Dcity=${city} -Dtag=${KEYWORD} -Dsqlite.file=${HOME}/twitter.db
+    done
 done
