8ae807713210934bb43106997f8afe4a29c97546
Yegor Bugayenko <yegor@tpc2.com>
2014-03-06 15:38:59 +0100

    paths fixed

--- cron.sh
+++ cron.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-HOME=$(dirname $0)
-cd ${HOME}/twitter-circles
 CITIES=(
     "52.3740300,4.8896900,30km",
     "37.4419444,-122.1419444,50mi"
 )
 for city in "${CITIES[@]}"
 do
-    mvn test -DskipTests -Plive -Pcuriost --settings ${HOME}/settings.xml \
-        -Dcity=${city} -Dtag=java -Dsqlite.file=${HOME}/twitter.db
+    mvn test -DskipTests -Plive -Pcuriost \
+        -Dcity=${city} -Dtag=java -Dsqlite.file=~/twitter.db
 done
