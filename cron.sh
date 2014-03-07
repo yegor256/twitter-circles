d526a817e54ce0cde03acec30c25fe010ab13cdd
Yegor Bugayenko <yegor@tpc2.com>
2014-03-07 11:29:11 +0100

    kill daemon before start

--- cron.sh
+++ cron.sh
@@ -13,6 +13,10 @@ OAUTH=$(cat ${DIR}/twitter.key)
 mvn --batch-mode --strict-checksums --errors --quiet \
     -Pliquibase -Dsqlite.file=${DB} clean package
 # http://software.clapper.org/daemonize/
+if [ -f ${DIR}/front.pid ]; then
+    kill -9 $(cat ${DIR}/front.pid)
+    rm -rf ${DIR}/front.pid
+fi
 daemonize -a -v -l ${DIR}/front.pid -p ${DIR}/front.pid -c ${DIR}/front \
     -o ${DIR}/stdout.log -e ${DIR}/stderr.log \
     $(which python) front.py ${DIR}/twitter.db
