015ea466c9b205f4694db28d9e9821711c177c07
Yegor Bugayenko <yegor@tpc2.com>
2014-03-07 11:17:28 +0100

    DIR everywhere

--- cron.sh
+++ cron.sh
@@ -3,21 +3,17 @@ set -e
 set -o pipefail
 
 cd $(dirname $0)
-source /etc/profile
-if [ ! -f /etc/init.d/functions ]; then
-    echo "You can run this script only in Linux"
-    exit -1;
-fi
+DIR=$(pwd)
 
 git pull
-DB=${HOME}/twitter.db
-OAUTH=$(cat ${HOME}/twitter.key)
+DB=${DIR}/twitter.db
+OAUTH=$(cat ${DIR}/twitter.key)
 mvn --batch-mode --strict-checksums --errors --quiet \
     -Pliquibase -Dsqlite.file=${DB} clean package
 # http://software.clapper.org/daemonize/
-daemonize -a -v -l ${HOME}/front.pid -p ${HOME}/front.pid -c ${HOME}/front \
-    -o ${HOME}/stdout.log -e ${HOME}/stderr.log \
-    python front.py ${HOME}/twitter.db
+daemonize -a -v -l ${DIR}/front.pid -p ${DIR}/front.pid -c ${DIR}/front \
+    -o ${DIR}/stdout.log -e ${DIR}/stderr.log \
+    python front.py ${DIR}/twitter.db
 
 CITIES=(
     "52.3740300,4.8896900,30km"
@@ -32,7 +28,7 @@ for keyword in "${KEYWORDS[@]}"
 do
     for city in "${CITIES[@]}"
     do
-        java -jar target/circles.jar "--city=${city}" \
+        java -jar ${DIR}/target/circles.jar "--city=${city}" \
             "--tag=${keyword}" "--jdbc=jdbc:sqlite:${DB}" "--key=${OAUTH}"
     done
 done
